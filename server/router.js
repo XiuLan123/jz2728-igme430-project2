b08048918d72627755e9c60446719a30c313181a
John Zhang <1724118498@qq.com>
2021-05-08 03:02:29 +0800

    push

--- server/router.js
+++ server/router.js
@@ -1,4 +1,10 @@
 const router = (app) => {
+
+    // put this AFTER the other `app.get(...`
+    // for any request that doesn't match one above, send back React's index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
 };
 
 module.exports = router;
